61c81f7363b5952da7af04743f8eb846e59523a8
ZhuMCh <305731523@qq.com>
2019-03-07 10:40:28 +0800

    http request config

--- src/api/http.js
+++ src/api/http.js
@@ -1,10 +1,43 @@
 import http from './axios'
 
-// get请求
-export function getReq(){
-    return http.fetchGet('')
+// 问题提报-列表
+export function problemList(page,size,desc,lineLike,dutyDeptLike){
+    return http.fetchPost('/newline/mobile/ProblemList',{
+        'page':page,
+        'size':size,
+        'description':desc,
+        'lineLike':lineLike,
+        'dutyDeptLike':dutyDeptLike
+    })
 }
-// post请求
-export function postReq(){
-    return http.fetchPost('',{})
+//问题提报-查看详情
+export function seeDetail(id){
+    return http.fetchGet('/newline/mobile/Problem',{'id':id})
+}
+//问题提报-批量删除
+export function delProblem(id){
+    return http.fetchGet('/newline/mobile/delProblem',{'ids':ids})
+}
+// 问题提报-新增
+export function addProblem(){
+    return http.fetchPost('/newline/mobile/addProblem',{
+
+    })
+}
+//问题提报-修改
+export function pdateProblem(id,...arge){
+    return http.fetchPost('/newline/mobile/addProblem',{
+        'id':id
+    })
+}
+//问题提报-提交
+export function homeSubmitProblem(ids){
+    return http.fetchGet('/newline/mobile/homePageProblemSubmit',{'ids':ids})
+}
+
+//问题提报-保存提交
+export function addSubmitProblem(){
+    return http.fetchPost('/newline/mobile/addProblemSubmit',{
+
+    })
 }
